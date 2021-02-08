6e160611121f30e5bd92030669118ebd6ba77103
staticvoidliam7 <63507206+staticvoidliam7@users.noreply.github.com>
2021-02-08 12:50:36 -0500

    Add files via upload

---
+++ src/main.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+
+class MainClass {
+  public static void Main () {
+    Console.WriteLine(">>");
+  }
+}
